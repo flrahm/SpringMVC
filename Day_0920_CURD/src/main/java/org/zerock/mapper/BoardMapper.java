de56d94ad412cc4aad65914dd0c4b1425bd18a50
flrahm <shss2216@gmail.com>
2019-09-23 17:25:55 +0900

    Day_0920

---
+++ Day_0920_CURD/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	//@Select("select * from tbl_board where bno > 0")
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO vo);
+	public void insertSelectKey(BoardVO vo);
+	public BoardVO read(long bno);
+	public int delete(Long bno);
+}
---
+++ Day_0920_CURD/src/test/java/org/zerock/persistence/dbtest.java
@@ -0,0 +1,36 @@
+package org.zerock.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class dbtest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	@Test
+	public void testConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "jsldb";
+		String password = "123123";
+
+		try {
+			Connection conn = DriverManager.getConnection(url, user, password);
+			log.info(conn);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
+
+}
